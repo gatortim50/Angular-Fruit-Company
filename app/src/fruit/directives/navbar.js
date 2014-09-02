5c3136e437190780c6a77caae0f5f16085bb2c40
Timothy McKee <railshead@gmail.com>
2014-09-02 00:25:09 -0400

    moved navbar header from html to directive

---
+++ app/src/fruit/directives/navbar.js
@@ -0,0 +1,14 @@
+(function (){
+
+  var navbar = function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'src/fruit/views/navbarHeader.html',
+      controller: 'fruitCtrl'
+    };
+
+  };
+
+  angular.module('demoApp').directive('navbar', navbar);
+
+}());
