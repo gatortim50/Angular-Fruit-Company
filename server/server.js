968f6465e4af9e9b9ae3b22ee77e71dc75cb447b
Timothy McKee <railshead@gmail.com>
2014-10-06 16:50:05 -0400

    added cors

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 var express = require('express'),
+  bodyParser = require('body-parser'),
+  logger = require('logger'),
   cors = require('cors');
-var product = require('./routes/routes');
 
+var product = require('./routes/product'); // product.js routes
  
 var app = express();
+
+app.use(bodyParser.json());
+
+//app.use(logger());     /* 'default', 'short', 'tiny', 'dev' */
+
 app.use(cors());
 
+// methods in routes/product.js
 app.get('/product', product.findAll);
 app.get('/product/:sku', product.findBySKU);
+app.post('/product', product.addProduct);
+app.put('/product/:id', product.updateProduct);
+app.delete('/product/:id', product.deleteProduct);
  
 
 var port = 3000;
