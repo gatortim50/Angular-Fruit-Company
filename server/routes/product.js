bf3f21ae2d6d47fb4d9d9a395e8dec80a34d9aa9
Timothy McKee <railshead@gmail.com>
2014-11-05 21:38:10 -0500

    modified sku to id

--- server/routes/product.js
+++ server/routes/product.js
@@ -12,43 +12,43 @@ exports.findById = function(req, res) {
 exports.findAll = function(req, res) {
   res.send([
     {
-      "sku": "5207A",
+      "id": "5207A",
       "name": "Red Delicious Apple",
       "color": "Red",
       "location": "USA"
     },
     {
-      "sku": "5206A",
+      "id": "5206A",
       "name": "Granny Apple",
       "color": "Green",
       "location": "Panama"
     },
     {
-      "sku": "5233O",
+      "id": "5233O",
       "name": "Valencia",
       "color": "Orange",
       "location": "Spain"
     },
     {
-      "sku": "2233M",
+      "id": "2233M",
       "name": "Mango",
       "color": "Red",
       "location": "Spain"
     },
     {
-      "sku": "3206G",
+      "id": "3206G",
       "name": "Green Seedless Grapes",
       "color": "Green",
       "location": "Panama"
     },
     {
-      "sku": "1206S",
+      "id": "1206S",
       "name": "Strawberries",
       "color": "Red",
       "location": "Spain"
     },
     {
-      "sku": "3216G",
+      "id": "3216G",
       "name": "Red Seedless Grapes",
       "color": "Red",
       "location": "Jamaica"
