04664ae62232c724bbcedb92f98c0d4b482d2340
Timothy McKee <railshead.mckee@gmail.com>
2016-02-16 00:06:17 -0500

    modified port number

--- server/server.js
+++ server/server.js
@@ -11,11 +11,11 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // invoke methods in routes/product.js
-app.get('/product', product.findProduct);     // http://127.0.0.1:3000/product
-app.get('/product/:id', product.findById);  // http://127.0.0.1:3000/product/1
-app.get('/products', product.findAll);        // http://127.0.0.1:3000/products
+app.get('/product', product.findProduct);     // http://127.0.0.1:4000/product
+app.get('/product/:id', product.findById);  // http://127.0.0.1:4000/product/1
+app.get('/products', product.findAll);        // http://127.0.0.1:4000/products
 
 
-var port = 3000;
+var port = 4000;
 app.listen(port);
 console.log('Server running at http://127.0.0.1:' + port);
