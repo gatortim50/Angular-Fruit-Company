104c63fd6b0f944e8b4ee7adcda5940f9a35cee3
Timothy McKee <timothy.mckee@viawest.com>
2016-04-01 22:24:40 -0400

    corrected port

--- server/server.js
+++ server/server.js
@@ -1,14 +1,28 @@
 var express = require('express'),
   bodyParser = require('body-parser'),
-  cors = require('cors');
+  cors = require('cors'),
+  morgan = require('morgan');
 
 var product = require('./routes/product');
 
 var app = express();
 
+// use body parser so we can get info from POST and/or URL parameters
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(cors());
+// use morgan to log requests to the console
+app.use(morgan('dev'));
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:9000');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+
+  next();
+});
 
 // invoke methods in routes/product.js
 app.get('/product', product.findProduct);     // http://127.0.0.1:4000/product
